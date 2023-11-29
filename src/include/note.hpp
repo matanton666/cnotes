3440e12775727dee58ad502ab714beec9d640fbf
matan Kabalo <matanton666@gmail.com>
2023-11-29 22:05:48 +0200

    added abstract functions to note class

--- src/include/note.hpp
+++ src/include/note.hpp
@@ -7,9 +7,14 @@ class Note
 {
 private:
     string _title;
+    unsigned int _id;
 public:
-    Note(string title);
+    Note(string title, unsigned int id);
     ~Note();
+
+    string getTitle() const;
+    unsigned int getId() const;
+    virtual void print() const = 0;
 };
 
 
--- src/note.cpp
+++ src/note.cpp
@@ -1,9 +1,19 @@
 #include "note.hpp"
 
-Note::Note(string title): _title(title)
+Note::Note(string title, unsigned int id): _title(title), _id(id)
 {
 }
 
 Note::~Note()
 {
+}
+
+string Note::getTitle() const
+{
+    return _title;
+}
+
+unsigned int Note::getId() const
+{
+    return _id;
 }
